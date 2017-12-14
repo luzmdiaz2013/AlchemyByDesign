b29bf63f17918577c900fa6622e90ea79f824a17
Luz Diaz <luzmdiaz2013@gmail.com>
2017-12-14 15:16:11 -0500

    completed Seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171214164503) do
+ActiveRecord::Schema.define(version: 20171214173208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20171214164503) do
     t.string "title"
     t.string "author_id"
     t.string "abstract"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "author_articles", force: :cascade do |t|
+    t.integer "author_id"
+    t.integer "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
