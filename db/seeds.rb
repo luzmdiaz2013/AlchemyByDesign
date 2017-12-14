3ff264d8bdf2a621a395095393f5363617b86332
Luz Diaz <luzmdiaz2013@gmail.com>
2017-12-14 15:56:55 -0500

    edit Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,15 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Author.create(name: 'Hernandez, D.', university: 'Columbia University', coAuthors: )
+Author.create(name: 'Hernandez, D.', university: 'Columbia University')
 Author.create(name: 'Hernandez, D.', university: 'Columbia University', coAuthors: 'Bird, S. ')
 Author.create(name: 'Hernandez, D.', university: 'Columbia University', coAuthors: 'Weinstein, H., & Munoz-Laboy, M.')
 
-Article.create( title: 'I’m gonna call my lawyer', author_id: , abstract: 'shifting legal consciousness at the intersection of inequality. In Special Issue Interdisciplinary Legal Studies: The Next Generation (pp. 95–121). doi:10.1108/s1059-4337(2010)0000051007')
-Article.create( title: 'Energy Burden and the Need for Integrated Low-Income Housing and Energy Policy', author_id:, abstract: 'Poverty & Public Policy, 2: 5–25. doi:10.2202/1944-2858.1095')
-Article.create( title: 'Youth perspectives on the intersections of violence, gender, and hip-hop', author_id:, abstract: 'Youth & Society, 44(4), 587-608. doi:10.1177/0044118X11408746')
+Article.create( title: 'I’m gonna call my lawyer', abstract: 'shifting legal consciousness at the intersection of inequality. In Special Issue Interdisciplinary Legal Studies: The Next Generation (pp. 95–121). doi:10.1108/s1059-4337(2010)0000051007')
+Article.create( title: 'Energy Burden and the Need for Integrated Low-Income Housing and Energy Policy', abstract: 'Poverty & Public Policy, 2: 5–25. doi:10.2202/1944-2858.1095')
+Article.create( title: 'Youth perspectives on the intersections of violence, gender, and hip-hop', abstract: 'Youth & Society, 44(4), 587-608. doi:10.1177/0044118X11408746')
+
+AuthorArticle.create(author_id: 1, article_id: 1)
+AuthorArticle.create(author_id: 2, article_id: 2)
+AuthorArticle.create(author_id: 3, article_id: 3)
+
