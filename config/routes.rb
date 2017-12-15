7833aec8be76b922672c00f0fc350316188faa98
Luz Diaz <luzmdiaz2013@gmail.com>
2017-12-15 16:02:59 -0500

    routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   get "/", to: "welcome#index"
   get "/about", to: "welcome#about"
   get "/projects", to: "welcome#projects"
+  get "/publications", to: "welcome#publications"
+  get "/videos", to: "welcome#videos"
+  get "/audio", to: "welcome#audio"
+  get "/contact", to: "welcome#contact"
+
 
 
   resources :articles
